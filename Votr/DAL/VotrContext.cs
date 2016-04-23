0bdd18cf12306c3d07e8dd7f5bfdffd1e8d07798
Jurnell Cockhren <jurnell@sophicware.com>
2016-04-23 10:01:36 -0700

    Added context and initial migration

---
+++ Votr/DAL/VotrContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using Votr.Models;
+
+namespace Votr.DAL
+{
+    public class VotrContext : DbContext
+    {
+        public DbSet<Poll> Polls { get; set; }
+        public DbSet<Option> Options { get; set; }
+    }
+}
