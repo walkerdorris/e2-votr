ef9d55f6dfcb523f0b0e1b548b34c507f601333b
Jurnell Cockhren <jurnell@sophicware.com>
2016-04-25 17:50:57 -0700

    Added Sandbox for exploring Moq usage.

---
+++ Votr.Tests/Sandbox/CarTest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Votr.Sandbox;
+using Moq;
+
+namespace Votr.Tests.Sandbox
+{
+    [TestClass]
+    public class CarTest
+    {
+        [TestMethod]
+        public void CarEnsureICanCreateInstance()
+        {
+            Car c = new Car();
+            Assert.IsNotNull(c);
+        }
+
+        [TestMethod]
+        public void CarEnsureICanHonk()
+        {
+            Car c = new Car();
+            Assert.AreEqual("Honk", c.Horn());
+        }
+
+        [TestMethod]
+        public void CarEnsureICanBeep()
+        {
+            Mock<Car> mock_car = new Mock<Car>();
+            mock_car.Setup(car => car.Horn()).Returns("Beep");
+
+            //Car c = new Car();
+            Car c = mock_car.Object;
+            Assert.AreEqual("Beep", c.Horn());
+
+        }
+
+        [TestMethod]
+        public void CarEnsureHonkWasCalled()
+        {
+            // Arrange
+            Mock<Car> mock_car = new Mock<Car>();
+            Car c = mock_car.Object;
+            mock_car.Setup(car => car.Horn()).Returns("Beep");
+
+            // Act
+            c.MakeNoise();
+
+            // Assert
+            mock_car.Verify(car => car.Horn(), Times.Once); // Look into.
+
+        }
+    }
+}
---
+++ Votr/Sandbox/Car.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Votr.Sandbox
+{
+    public class Car
+    {
+        virtual public string Horn()
+        {
+            return "Honk";
+        }
+
+        virtual public void MakeNoise()
+        {
+            Horn();
+        }
+    }
+}
