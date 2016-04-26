1e61114474142f57cebca9959852f5bc62eac9fd
Jurnell Cockhren <jurnell@sophicware.com>
2016-04-25 19:37:07 -0700

    tests for AddPoll, GetPolls, GetPollCount. Tests fail due to lack of mocking.

---
+++ Votr.Tests/DAL/VotrRepositoryTest.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Votr.DAL;
+using System.Collections.Generic;
+using Votr.Models;
+
+namespace Votr.Tests.DAL
+{
+    [TestClass]
+    public class VotrRepositoryTest
+    {
+        [TestMethod]
+        public void RepoEnsureICanCreateInstance()
+        {
+            VotrRepository repo = new VotrRepository();
+            Assert.IsNotNull(repo);
+        }
+
+        [TestMethod]
+        public void RepoEnsureIsUsingContext()
+        {
+            // Arrange 
+            VotrRepository repo = new VotrRepository();
+
+            // Act
+
+            // Assert
+            Assert.IsNotNull(repo.context);
+        }
+
+        [TestMethod]
+        public void RepoEnsureThereAreNoPolls()
+        {
+            // Arrange 
+            VotrRepository repo = new VotrRepository();
+
+            // Act
+            List<Poll> list_of_polls = repo.GetPolls();
+            List<Poll> expected = new List<Poll>();
+
+            // Assert
+            Assert.AreEqual(expected.Count, list_of_polls.Count);
+        }
+
+        [TestMethod]
+        public void RepoEnsurePollCountIsZero()
+        {
+            // Arrange 
+            VotrRepository repo = new VotrRepository();
+
+            // Act
+            int expected = 0;
+            int actual = repo.GetPollCount();
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void RepoEnsureICanAddPoll()
+        {
+            // Arrange
+            VotrRepository repo = new VotrRepository();
+
+            // Act
+            repo.AddPoll("Some Title", DateTime.Now, DateTime.Now); // Not there yet.
+            int actual = repo.GetPollCount(); 
+            int expected = 1;
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ Votr/DAL/VotrRepository.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Votr.Models;
+
+namespace Votr.DAL
+{
+    public class VotrRepository
+    {
+        public VotrContext context { get; set; }
+
+        public VotrRepository()
+        {
+            // We need an instance of a Context
+            context = new VotrContext();
+        }
+
+        public int GetPollCount()
+        {
+            //return GetPolls().Count;
+            // Another way
+            return context.Polls.Count();
+        }
+
+        public List<Poll> GetPolls()
+        {
+            return context.Polls.ToList<Poll>();
+        }
+
+        public void AddPoll(string title, DateTime start_time, DateTime end_time)
+        {
+            throw new NotImplementedException();
+        }
+        // Create a Poll
+
+        // Delete a Poll
+
+        // Vote
+    }
+}
